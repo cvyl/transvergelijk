4bef56b74dcdcfec66f87f26ddd6ad05b22c9381
跨性别 <cvyl@pm.me>
2024-06-01 22:25:18 +0200

    [F] Pre-render fix

--- src/app/api/fetchData/route.ts
+++ src/app/api/fetchData/route.ts
@@ -3,10 +3,6 @@ import { scrapeData } from '../../utils/scrapeData';
 import { HospitalData } from '../../../types/hospitalData';
 
 export async function GET(request: NextRequest) {
-  try {
     const data: HospitalData[] = await scrapeData();
     return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.error();
-  }
 }
